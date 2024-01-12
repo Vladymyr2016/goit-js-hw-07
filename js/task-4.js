8dc6fb0a1a76aeddae5142e0a774505a4b809c9b
Vladymyr2016 <142669270+Vladymyr2016@users.noreply.github.com>
2024-01-12 17:17:00 +0100

    t4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,18 @@
+const formEl = document.querySelector(".login-form");
+console.log(formEl);
+const emailValue = document.querySelector("textContent");
+console.log(emailValue);
 
+function onSubmit(event) {
+  event.preventDefault();
+  //   console.log(event.target.elements.email.value);
+  const { email, password } = event.target.elements;
+  if (!email.value.trim() || !password.value.trim()) {
+    return alert("All form fields must be filled in");
+  }
+
+  console.log({ email: email.value.trim(), password: password.value.trim() });
+  event.currentTarget.reset();
+}
+
+formEl.addEventListener("submit", onSubmit);
