84ee97b1706a675632366fac3fb2c48c1e0d9cc5
Vladymyr2016 <142669270+Vladymyr2016@users.noreply.github.com>
2024-01-12 17:46:09 +0100

    t5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,18 @@
+const btnEl = document.querySelector(".change-color");
+const spanEl = document.querySelector(".color");
+console.log(document.body);
+const bodyEl = document.body;
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function onColorChange(event) {
+  const currentColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = currentColor;
+  spanEl.textContent = currentColor;
+}
+
+btnEl.addEventListener("click", onColorChange);
